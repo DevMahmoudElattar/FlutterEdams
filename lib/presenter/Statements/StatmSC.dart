bfd9cbb49c24a33799f835a568c36d2fb4b9c0ac
mahmoud Elattar <egy.dev.mahmoud@gmail.com>
2023-04-04 12:26:17 +0200

    Statms info screen start

--- lib/presenter/Statements/StatmSC.dart
+++ lib/presenter/Statements/StatmSC.dart
@@ -1,3 +1,4 @@
+import 'package:customers_app/presenter/Statements/Info/STInfo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,8 +19,8 @@ class StatmSC extends StatelessWidget {
               decoration: const BoxDecoration(boxShadow: [
                 BoxShadow(
                     color: Colors.grey,
-                    blurRadius: 2,
-                    spreadRadius: 2,
+                    blurRadius: 7,
+                    spreadRadius: 1,
                     offset: Offset(0, 5))
               ]),
               child: Column(
@@ -98,15 +99,71 @@ class StatmSC extends StatelessWidget {
                               child: Container(
                                 width: Sizes.wGeneral(context, .5),
                                 decoration: const BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
                                   color: Colors.blue,
                                 ),
-
                               )),
+                          const VerticalDivider(thickness: 0,width: 5,color: Colors.transparent,),
                           Expanded(
                               flex: 20,
-                              child: Container(
-                                color: Colors.yellow,
+                              child: GestureDetector(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const STInfo()));
+                                },
+                                child: Container(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      color : Colors.white,
+                                      boxShadow: [...OtherSharedValues.SectionShadow]
+                                    ),
+                                    child: Column(
+                                      children:   [
+                                        const Expanded(flex: 1,child: Center(child: Text("MAR 23"))),
+                                        const Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 10),
+                                          child: Divider(color: Colors.blueGrey,thickness: 0,height: 0,),
+                                        ),
+                                        Expanded(flex: 2,child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          children: [
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Text("Balance",style: TextStyle(color: ColorsVal.mainCardBG),),
+                                                Text("R17",style: TextStyle(color: ColorsVal.mainCardBG,fontWeight: FontWeight.bold))
+                                              ],
+                                            ),
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Text("Payment",style: TextStyle(color: ColorsVal.mainCardBG),),
+                                                Text("49",style: TextStyle(color: ColorsVal.mainCardBG,fontWeight: FontWeight.bold))
+                                              ],
+                                            ),
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Text("Transactions",style: TextStyle(color: ColorsVal.mainCardBG),),
+                                                Text("17",style: TextStyle(color: ColorsVal.mainCardBG,fontWeight: FontWeight.bold))
+                                              ],
+                                            ),
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Text("OP",style: TextStyle(color: ColorsVal.mainCardBG),),
+                                                Text("50",style: TextStyle(color: ColorsVal.mainCardBG,fontWeight: FontWeight.bold))
+                                              ],
+                                            ),
+
+                                          ],
+                                        ))
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ))
                         ],
                       ),
